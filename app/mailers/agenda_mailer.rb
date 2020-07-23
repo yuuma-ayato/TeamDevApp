fd8ba7396f5ba388a0ae607593c6c802bd5ffdc3
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-07-23 09:59:29 +0900

    インデント修正

--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -1,7 +1,7 @@
 class AgendaMailer < ApplicationMailer
   def agenda_deleted_mail(member,agenda)
-  @member = member
-  @agenda = agenda
-  mail to: member.email, subject: I18n.t('views.messages.destroy_agenda')
-end
+    @member = member
+    @agenda = agenda
+    mail to: member.email, subject: I18n.t('views.messages.destroy_agenda')
+  end
 end
