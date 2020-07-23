bbdf7aebb4bb3570ecc1c72550f38c25c3197664
akihiro tokuhara <muscletown_mcm@yahoo.co.jp>
2020-07-23 09:07:37 +0900

    agenda削除時のメール送信機能を修正

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,5 +1,5 @@
 class AgendasController < ApplicationController
-  #before_action :set_agenda, only: %i[destroy]
+  before_action :set_agenda, only: %i[show edit update destroy]
 
   def index
     @agendas = Agenda.all
--- app/mailers/agenda_mailer.rb
+++ app/mailers/agenda_mailer.rb
@@ -2,6 +2,6 @@ class AgendaMailer < ApplicationMailer
   def agenda_deleted_mail(member,agenda)
   @member = member
   @agenda = agenda
-  mail to: @agenda.email, subject: I18n.t('views.messages.destroy_agenda')
+  mail to: member.email, subject: I18n.t('views.messages.destroy_agenda')
 end
 end
